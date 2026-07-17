bd733c59956f6185eb965a0803daced705e7d07a
Abstract <21150449+abstractpotato@users.noreply.github.com>
2026-07-16 22:47:47 -0400

    Jul 16, 2026 - 10:47 PM

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -5,6 +5,33 @@ import (
   Ledger "github.com/abstractpotato/starch-pay-ledger"
 )
 
-func AddBlock() {
-
+type Consensus struct {
+  Ledger Ledger.Ledger
+  Disk   Ledger.Disk
 }
+
+func NewConsensus() Consensus {
+  return {
+    Ledger: Ledger.NewLedger(),
+    Disk: Ledger.NewDisk(),
+  }
+}
+
+func (consensus *Consensus) AddBlock(block PSL.Block) {
+  // check if disk has block
+  // if not check if block is valid
+    // calc the epoch
+    // verify the leader has signed
+    //
+}
+
+func (consensus *Consensus) UpdateWitnesses(blockID uint, witness PSL.Witness) {
+  // verify cert
+  // add witness if valid
+}
+
+// func (consensus *Consensus) ValidateBlock(block PSL.Block) bool {}
+
+// func (consensus *Consensus) ValidateTx(tx PSL.Transaction) bool {}
+
+// func (consensus *Consensus) DetermineLeader(id uint) {}
